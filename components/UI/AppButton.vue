7b2351d38364d32859f264da4f391a5b2e6b0b66
riana randoja <rianarandoja@gmail.com>
2018-12-30 01:09:59 +0200

    UI Improvements

---
+++ components/UI/AppButton.vue
@@ -0,0 +1,63 @@
+<template>
+  <button
+    :class="btnStyle"
+    v-bind="$attrs"
+    class="button"
+    v-on="$listeners"><slot /></button>
+</template>
+
+<script>
+export default {
+  name: 'AppButton',
+  props: {
+    btnStyle: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+
+<style scoped lang="scss">
+.button {
+  font: inherit;
+  background-color: $dark-main;
+  color: white;
+  padding: 4px;
+  cursor: pointer;
+  border: 1px solid $dark-main;
+}
+
+.button:hover,
+.button:active {
+  background-color: $dark-admin;
+  border: 1px solid $dark-admin;
+}
+
+.inverted {
+  background-color: white;
+  color: black;
+  border: 1px solid transparent;
+}
+
+.inverted:hover,
+.inverted:active {
+  color: #c5c5c5;
+  background-color: white;
+  border: 1px solid transparent;
+}
+
+.cancel {
+  background-color: #c5c5c5;
+  color: black;
+  border: 1px solid transparent;
+}
+
+.cancel:hover,
+.cancel:active {
+  background-color: #d9d9d9;
+  border: 1px solid transparent;
+  color: black;
+}
+</style>
---
+++ layouts/error.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="error-page">
+    <h2>Oops! Something went terribly wrong!</h2>
+    <p>Back to <a href="/">the safe side</a>!</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.error-page {
+  text-align: center;
+  font-weight: bold;
+}
+
+.error-page a {
+  text-decoration: none;
+  color: $orange;
+}
+
+.error-page a:hover,
+.error-page a:active {
+  color: #e7725a;
+}
+</style>
